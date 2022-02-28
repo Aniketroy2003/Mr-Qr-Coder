4b23612d2a86b0849b1ae3578c85c060fb767f73
Aniketroy2003 <73846184+Aniketroy2003@users.noreply.github.com>
2022-02-28 22:29:28 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware,
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'qrcoder/templates')
